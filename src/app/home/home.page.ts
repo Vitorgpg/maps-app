d09def1db0b82a05030ffcea37364274a8e5183e
Alexsander Gonçalves da Silva <alexsander.gon.silva@gmail.com>
2023-10-24 20:33:47 -0300

    Aula 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  // Pega o elemento com ID map do HTML
+  @ViewChild('map') mapRef!: ElementRef;
+
+  // Cria a variavel do Maps
+  map!: google.maps.Map;
+
   constructor() {}
+
+  async exibirMapa(){
+
+    // The location of Uluru
+    const position = { lat: -22.489201, lng: -48.546444 };
+
+    // Request needed libraries.
+    //@ts-ignore
+    const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+    // const { AdvancedMarkerView } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+
+    // The map, centered at Uluru
+    this.map = new Map(
+      this.mapRef.nativeElement,
+      {
+        zoom: 20,
+        center: position,
+        mapId: 'DEMO_MAP_ID',
+      }
+    );
+
+    /* The marker, positioned at Uluru
+    const marker = new AdvancedMarkerView({
+      map: map,
+      position: position,
+      title: 'Uluru'
+    });
+    */
+  }
+
+  ionViewWillEnter(){
+    this.exibirMapa();
+  }
 
 }
